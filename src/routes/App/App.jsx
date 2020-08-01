ab85fff57d066949547f77faf34faf11f400c10e
Mário Prada <mariosilvaprada@gmail.com>
2020-08-01 14:36:30 +0100

    updating

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './App.styled';
 
-const App = () => (
-  <S.Container>
-    <h1>Home</h1>
-  </S.Container>
-);
+const App = () => {
+  const getVideo = () => {
+    window.addEventListener('load', () => {
+      const CONTENT_ID = '179058_c_559455';
+      dacast(CONTENT_ID, 'video', {
+        width: 800,
+        height: 600,
+      });
+    });
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, []);
+
+  return (
+    <S.Container>
+      <div id="video" />
+    </S.Container>
+  );
+};
 
 export default App;
--- src/routes/App/App.styled.jsx
+++ src/routes/App/App.styled.jsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`height: 100%;`;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #bdd4e7;
+  background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+`;
 
 export const Title = styled.div`color: red;`;

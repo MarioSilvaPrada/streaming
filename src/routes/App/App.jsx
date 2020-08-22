474462e72fdf04d7a41eaf01164424ec935d8d63
Mário Prada <mariosilvaprada@gmail.com>
2020-08-22 08:17:17 +0100

    updating

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -1,29 +1,18 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import config from '../../config';
 import * as S from './App.styled';
 
 const App = () => {
   const { welcomeText } = config;
-  const { pathname } = useLocation();
 
-  const getVideo = () => {
-    window.addEventListener('load', () => {
-      const CONTENT_ID = '179058_c_559516';
-      dacast(CONTENT_ID, 'video', {
-        width: 500,
-        height: 300,
-        position: 'relative',
-      });
+  useEffect(() => {
+    const CONTENT_ID = '179058_c_559516';
+    dacast(CONTENT_ID, 'video', {
+      width: 500,
+      height: 300,
+      position: 'relative'
     });
-  };
-
-  useEffect(
-    () => {
-      getVideo();
-    },
-    [pathname],
-  );
+  }, []);
   return (
     <S.Container>
       <S.Title>{welcomeText}</S.Title>

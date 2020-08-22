5718c707ece6734c5461ddf3bbb9cb536ce96afd
Mário Prada <mariosilvaprada@gmail.com>
2020-08-22 09:19:29 +0100

    update

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -4,13 +4,13 @@ import * as S from './App.styled';
 
 const App = () => {
   const { welcomeText } = config;
+  const CONTENT_ID = '179058_c_559516';
 
   useEffect(() => {
-    const CONTENT_ID = '179058_c_559516';
     dacast(CONTENT_ID, 'video', {
       width: 500,
       height: 300,
-      position: 'relative',
+      position: 'relative'
     });
   }, []);
   return (
